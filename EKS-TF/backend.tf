33903b5ae9efa1e7fe6547b08f296b44408ab199
ASHUTOSH BHOSALE <bhosaleashutosh262@gmail.com>
2026-09-04 16:23:18 +0530

    Add Terraform backend configuration for S3

---
+++ EKS-TF/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "ashu1626" # Replace with your actual S3 bucket name
+    key    = "EKS/terraform.tfstate"
+    region = "ap-south-1"
+  ##  profile = "eks"
+  }
+}
